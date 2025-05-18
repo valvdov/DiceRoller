573da138aa1ea666a0e5ab99020cef198d8a6916
Valerii <valvdov@gmail.com>
2025-05-18 23:39:53 +0100

    Finish project

---
+++ DiceRoller/DiceView.swift
@@ -0,0 +1,33 @@
+//
+//  DiceView.swift
+//  DiceRoller
+//
+//  Created by Valera Vdovin on 18/05/2025.
+//
+
+import SwiftUI
+
+struct DiceView: View {
+    @State private var numberOfPips: Int = 1
+    
+    var body: some View {
+        VStack {
+            Image(systemName: "die.face.\(numberOfPips).fill")
+                .resizable()
+                .frame(maxWidth: 100, maxHeight: 100)
+                .aspectRatio(1, contentMode: .fit)
+                .foregroundStyle(.black, .white)
+            
+            Button("Roll") {
+                withAnimation {
+                    numberOfPips = Int.random(in: 1...6)
+                }
+            }
+            .buttonStyle(.bordered)
+        }
+    }
+}
+
+#Preview {
+    DiceView()
+}
